ba8b70e0619d3cb95c5778fcc2ef6a74021da534
berly_aditya <aditalfarizi732007@gmail.com>
2026-03-16 23:10:12 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: " . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Admin Dashboard";
+    });
+    Route::get('/users', function () {
+        return "Admin Users";
+    });
+});
+
+Route::get('/listbarang/{id}/{nama}', [App\Http\Controllers\ListBarangController::class, 'tampilkan']);
