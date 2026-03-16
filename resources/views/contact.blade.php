f04591441da026817170e0bf342ab187bd10b3d6
berly_aditya <aditalfarizi732007@gmail.com>
2026-03-16 22:10:13 +0700

    ubah

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+     Silahkan hubungi kami untuk informasi lebih lanjut tentang produk dan layanan kami.
+</div>
